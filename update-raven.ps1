0dfed44640bad3fe6cb72e45816a6f1e5cb17277
Hudson Cavazin <hudson@zins.com.br>
2020-08-18 08:27:23 -0300

    ajustes Write-Host

--- update-raven.ps1
+++ update-raven.ps1
@@ -35,26 +35,26 @@ if (!(Test-Path "c:\ravendb")) {
 
 Set-Location "c:\ravendb"
 
-Write-Host "Baixando RavenDB"
+Write-Host "Baixando RavenDB..."
 Invoke-WebRequest "https://daily-builds.s3.amazonaws.com/RavenDB-$versaoRaven-windows-x64.zip" -OutFile "_download.zip"
 
-Write-Host "Parando Zins Processor"
+Write-Host "Parando Zins Processor..."
 taskkill /F /T /IM ZinsProcessor.exe
 
-Write-Host "Parando RavenDB"
+Write-Host "Parando RavenDB..."
 net stop ravendb
 taskkill /F /T /IM Raven.Server.exe
 timeout 3
 
-Write-Host "Extraindo arquivos"
+Write-Host "Extraindo arquivos..."
 Unzip "c:\ravendb\_download.zip" "c:\ravendb"
 
 [System.Environment]::SetEnvironmentVariable('RAVEN_Http_Protocols', 'Http1', [System.EnvironmentVariableTarget]::Machine)
 
-Write-Host "Iniciando RavenDB"
+Write-Host "Iniciando RavenDB..."
 net start ravendb
 
-Write-Host "Iniciando Zins Processor"
+Write-Host "Iniciando Zins Processor..."
 Start-Process "$env:LOCALAPPDATA\ZinsProcessor2\ZinsProcessor.exe"
 
 Start-Process "https://a.generic.ravendb.zinsc.com:883/studio/index.html#databases/documents?&database=Zins2"
