75f880fd92a940b7b390fd1969d9e8ff128df6f1
Hudson Cavazin <hudson@zins.com.br>
2020-08-20 20:00:04 -0300

    update camserver

---
+++ update-camserver.ps1
@@ -0,0 +1,22 @@
+
+# https://stackoverflow.com/a/9949909/4862220
+# $ErrorActionPreference = "Stop"
+
+if (!(Test-Path "C:\Zins\ZinsCamServer")) {
+    New-Item "C:\Zins\ZinsCamServer" -ItemType Directory
+}
+
+Write-Host "Baixando CamServer..."
+Invoke-WebRequest "http://f.zins.com.br/updates/zins/ZinsCamServer.zip" -OutFile "C:\Zins\ZinsCamServer\_download.zip"
+
+Write-Host "Parando CamServer..."
+net stop ZinsCamServer
+timeout 3
+
+Write-Host "Extraindo arquivos..."
+Expand-Archive "C:\Zins\ZinsCamServer\_download.zip" "C:\Zins\ZinsCamServer" -Force
+
+Write-Host "Iniciando CamServer..."
+net start ZinsCamServer
+
+Start-Process "https://localhost:9981/home/versao"
