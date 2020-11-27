7bdc28208458e60c7e4a04be9f37297a91d5e75f
Hudson Cavazin <hudson@zins.com.br>
2020-11-27 10:26:44 -0300

    ravendb 5.1.0

--- update-raven.ps1
+++ update-raven.ps1
@@ -2,7 +2,7 @@
 # https://stackoverflow.com/a/9949909/4862220
 # $ErrorActionPreference = "Stop"
 
-$versaoRaven = "5.0.5"
+$versaoRaven = "5.1.0"
 
 if (!(Test-Path "c:\ravendb")) {
     New-Item "c:\ravendb" -ItemType Directory
