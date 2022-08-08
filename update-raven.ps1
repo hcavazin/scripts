470623385b4a2529ac828d14e3e9e97e0a8fee18
Hudson Cavazin <hudson@zins.com.br>
2022-08-08 16:12:12 -0300

    ravendb 5.4.1

--- update-raven.ps1
+++ update-raven.ps1
@@ -2,7 +2,7 @@
 # https://stackoverflow.com/a/9949909/4862220
 # $ErrorActionPreference = "Stop"
 
-$versaoRaven = "5.1.10"
+$versaoRaven = "5.4.1"
 Write-Host "Versao RavenDB: $versaoRaven"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
