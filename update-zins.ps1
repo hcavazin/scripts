149e54c441699f7d49f4b0ed79b5b7b237135090
Hudson Cavazin <hudson@zins.com.br>
2024-03-25 14:56:54 -0300

    Atualização do script de atualização do Zins

--- update-zins.ps1
+++ update-zins.ps1
@@ -4,6 +4,9 @@ param (
     [string]$Version
 )
 
+# stop on error
+$ErrorActionPreference = "Stop"
+
 # ir para diretorio C:\Zins
 Set-Location C:\Zins
 
@@ -12,15 +15,27 @@ $7zaUrl = "https://zinspublic.blob.core.windows.net/updates/utils/7za_x64.exe"
 $7zaPath = "C:\Zins\7za.exe"
 Invoke-WebRequest -Uri $7zaUrl -OutFile $7zaPath
 
+# funcao para remover diretorio, caso exista
+function Remove-Directory {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+
+    if (Test-Path $Path) {
+        Remove-Item -Path $Path -Recurse -Force
+    }
+}
+
 # remover pastas antigas: Processor2, Transceiver2 e Workstation2
-Remove-Item -Path "C:\Zins\Processor2" -Recurse -Force
-Remove-Item -Path "C:\Zins\Transceiver2" -Recurse -Force
-Remove-Item -Path "C:\Zins\Workstation2" -Recurse -Force
+Remove-Directory -Path "C:\Zins\Processor2"
+Remove-Directory -Path "C:\Zins\Transceiver2"
+Remove-Directory -Path "C:\Zins\Workstation2"
 
 # remover pastas novas: ZinsProcessor, ZinsTransceiver, ZinsWorkstation
-Remove-Item -Path "C:\Zins\ZinsProcessor" -Recurse -Force
-Remove-Item -Path "C:\Zins\ZinsTransceiver" -Recurse -Force
-Remove-Item -Path "C:\Zins\ZinsWorkstation" -Recurse -Force
+Remove-Directory -Path "C:\Zins\ZinsProcessor"
+Remove-Directory -Path "C:\Zins\ZinsTransceiver"
+Remove-Directory -Path "C:\Zins\ZinsWorkstation"
 
 $baseUrlTransceiver = "https://zinspublic.blob.core.windows.net/updates/zins/ZinsTransceiver-$Version.7z"
 $baseUrlProcessor = "https://zinspublic.blob.core.windows.net/updates/zins/ZinsProcessor-$Version.7z"
@@ -32,28 +47,36 @@ $downloadPathProcessor = "C:\Zins\ZinsProcessor-$Version.7z"
 $downloadPathWorkstation = "C:\Zins\ZinsWorkstation-$Version.7z"
 $downloadPathCamServer = "C:\Zins\ZinsCamServer-$Version.7z"
 
+
 # download
+Write-Host "Downloading $baseUrlTransceiver"
 Invoke-WebRequest -Uri $baseUrlTransceiver -OutFile $downloadPathTransceiver
+
+Write-Host "Downloading $baseUrlProcessor"
 Invoke-WebRequest -Uri $baseUrlProcessor -OutFile $downloadPathProcessor
+
+Write-Host "Downloading $baseUrlWorkstation"
 Invoke-WebRequest -Uri $baseUrlWorkstation -OutFile $downloadPathWorkstation
+
+Write-Host "Downloading $baseUrlCamServer"
 Invoke-WebRequest -Uri $baseUrlCamServer -OutFile $downloadPathCamServer
 
-# parar servico ZinsCamServer
-Stop-Service -Name ZinsCamServer
+
+# rodar comando "Stop-Service -Name ZinsCamServer" como administrador
+Start-Process powershell -Verb RunAs -ArgumentList "-Command Stop-Service -Name ZinsCamServer"
 
 # extrair arquivos
-$unzipPath = "C:\Zins"
-& $7zaPath x $downloadPathTransceiver -o$unzipPath -y
-& $7zaPath x $downloadPathProcessor -o$unzipPath -y
-& $7zaPath x $downloadPathWorkstation -o$unzipPath -y
-& $7zaPath x $downloadPathCamServer -o$unzipPath -y
+& $7zaPath x $downloadPathTransceiver -o"C:\Zins" -y
+& $7zaPath x $downloadPathProcessor -o"C:\Zins" -y
+& $7zaPath x $downloadPathWorkstation -o"C:\Zins" -y
+& $7zaPath x $downloadPathCamServer -o"C:\Zins" -y
 
-# unzip
-$unzipPath = "C:\Zins"
-Expand-Archive -Path $downloadPath -DestinationPath $unzipPath
+# rodar comando "Start-Service -Name ZinsCamServer" como administrador
+Start-Process powershell -Verb RunAs -ArgumentList "-Command Start-Service -Name ZinsCamServer"
 
-# remove zip
-Remove-Item $downloadPath
 
-# iniciar servico ZinsCamServer
-Start-Service -Name ZinsCamServer
+# remover arquivos baixados
+Remove-Item -Path $downloadPathTransceiver
+Remove-Item -Path $downloadPathProcessor
+Remove-Item -Path $downloadPathWorkstation
+Remove-Item -Path $downloadPathCamServer
